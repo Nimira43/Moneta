1f3bea44d30902761a739e8c3ac7cabfd41052ba
Nimira43 <n.rabone72@aol.com>
2025-05-03 18:34:30 +0100

    Script -2

--- script.js
+++ script.js
@@ -7,3 +7,11 @@ document.querySelectorAll('.login').forEach(loginBtn => {
 document.querySelectorAll('.signup').forEach(signupBtn => {
 
 })
+
+const dropdownItems = document.querySelectorAll('.dropdown-hover')
+
+if (window.innerWidth < 1000) { }
+
+window.addEventListener('resize', () => {
+  window.location.reload()
+})
