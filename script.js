eab7e2b2d328a42ae47e6088dfe2f8a787d5740c
Nimira43 <n.rabone72@aol.com>
2025-05-03 18:32:15 +0100

    Script - 1

---
+++ script.js
@@ -0,0 +1,9 @@
+document.querySelectorAll('.logo').forEach(logo => {
+  
+})
+document.querySelectorAll('.login').forEach(loginBtn => {
+
+})
+document.querySelectorAll('.signup').forEach(signupBtn => {
+
+})
