c26f59c56e36193b48aa340f4bd2ff45f7a25a59
Nimira43 <n.rabone72@aol.com>
2025-05-03 19:51:52 +0100

    Script - 7

--- script.js
+++ script.js
@@ -26,13 +26,22 @@ if (window.innerWidth < 1000) {
   const menuIcon = document.querySelector('.menu')
   const navbar = document.querySelector('.navbar')
 
+  menuIcon.addEventListener('click', () => {
+    navbar.classList.toggle('change')
+
+    if (!navbar.classList.contains('change')) {
+      document.querySelectorAll('.nav-dropdown').forEach(dropdown => {
+        dropdown.style.left = '-20rem'
+      })
+    }
+  })
 
   document.querySelectorAll('.show-dropdown').forEach(link => {
     link.addEventListener('click', () => {
       link.nextElementSibling.style.left = '0'
     })
   })
-  document.querySelectorAll('.dropdown-heafing-link').forEach(headingLink => {
+  document.querySelectorAll('.dropdown-heading-link').forEach(headingLink => {
     headingLink.addEventListener('click', () => {
       headingLink.parentElement.parentElement.style.left = '-20rem'
     })
