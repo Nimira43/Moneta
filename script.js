717ef26e245ebe5f88c40973d20462a37af958cc
Nimira43 <n.rabone72@aol.com>
2025-05-03 18:38:37 +0100

    Script -3

--- script.js
+++ script.js
@@ -1,16 +1,30 @@
 document.querySelectorAll('.logo').forEach(logo => {
-  
+  logo.addEventListener('click', () => {
+    document.querySelector('front-page').computedStyleMap.display = 'block'
+    document.querySelector('login-page').computedStyleMap.display = 'none'
+    document.querySelector('signup-page').computedStyleMap.display = 'none'
+  })
 })
 document.querySelectorAll('.login').forEach(loginBtn => {
-
+  loginBtn.addEventListener('click', () => {
+    
+  })
 })
 document.querySelectorAll('.signup').forEach(signupBtn => {
-
+  signupBtn.addEventListener('click', () => {
+    
+  })
 })
 
 const dropdownItems = document.querySelectorAll('.dropdown-hover')
 
-if (window.innerWidth < 1000) { }
+if (window.innerWidth < 1000) { 
+  const menuIcon = document.querySelector('.menu')
+  const navbar = document.querySelector('.navbar')
+
+} else {
+
+}
 
 window.addEventListener('resize', () => {
   window.location.reload()
